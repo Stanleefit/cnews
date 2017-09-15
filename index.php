060d6828fe4ef8aba257d556115272e08e0ee323
Stanleefit <stanleefit@gmail.com>
2017-09-14 23:15:05 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+Name: CNews
+Author Name: Stanley Lalanne
+Author URI : http://stanleylalanne.com
+
+*/
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome To CNews Network </title>
+</head>
+<body>
+
+</body>
+</html>
